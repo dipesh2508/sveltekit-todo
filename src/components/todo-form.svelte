44dd317c91da420bb03fec118364dcac3b0c1517
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2023-11-10 20:50:16 +0530

    added todoStore

---
+++ src/components/todo-form.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {addTodo} from '../stores/todoStore'
+	let todo = '';
+
+	const handleSubmit = () => {
+        addTodo(todo);
+        todo = '';
+		console.log('submitting');
+	};
+</script>
+
+<form class="my-6" on:submit|preventDefault={handleSubmit}>
+	<div class="flex flex-col text-sm mb-2">
+		<label for="todo" class="font-bold mb-2 text-slate-800">Todo</label>
+		<input
+			type="text"
+			name="todo"
+			placeholder="Add a todo"
+			bind:value={todo}
+			class="apperance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-slate-400 rounded-lg"
+		/>
+	</div>
+	<button
+		type="submit"
+		class="w-full shadow-sm rounded bg-emerald-500 border-2 border-emerald-500 hover:bg-transparent hover:text-emerald-500 text-slate-50 py-2 px-4"
+		>submit</button
+	>
+</form>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import TodoForm from '../components/todo-form.svelte';
+    import Todo from '../components/todo-form.svelte';
+    import { todos } from '../stores/todoStore'
+  </script>
+  
+  
+  <main>
+      <h1 class="text-2xl md:text-3xl font-bold text-center text-slate-800">
+          My Todos
+      </h1>
+      <TodoForm />
+      {#each $todos as todo, index}
+        <Todo todo={todo} index={index} />
+      {/each}
+  </main>
+    
+    <style lang="postcss">
+      :global(html) {
+        background-color: theme(colors.gray.100);
+      }
+    </style>
