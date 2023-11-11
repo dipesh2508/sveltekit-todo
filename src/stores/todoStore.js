3e6935308fe84cf36482801cf14774fbe732bb5e
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2023-11-12 03:28:16 +0530

    updated todoStore

--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -1,30 +1,56 @@
 import { writable } from 'svelte/store';
-
+import { supabase } from '../supabase';
 export const todos = writable([]);
 
-export const addTodo = (text) => {
-    todos.update((cur) => {
-        const newTodos = [...cur, { text, completed: false, id: Date.now() }];
-        return newTodos;
-    });
+export const loadTodos = async () => {
+	const { data, error } = await supabase.from('todos').select();
+
+	if (error) {
+		return console.error(error);
+	}
+
+	todos.set(data);
 };
 
-export const deleteTodo = (id) => {
-	todos.update((todos) => todos.filter((todo) => todo.id !== id));
+export const addTodo = async (text, user_id = 'text') => {
+	const { data, error } = await supabase.from('todos').insert([{ text , user_id}]);
+
+	if (error) {
+		return console.error(error);
+	}
+
+	todos.update((cur) => [...cur, data[0]]);
 };
 
-export const toggleTodoCompleted = (id) => {
-    todos.update((todos) => {
-       let index = -1;
-       for(let i = 0; i < todos.length; i++) {
-        if(todos[i].id === id){
-            index = i;
-            break;
-        }
-       }
-       if (index !== -1) {
-           todos[index].completed = !todos[index].completed;
-       }
-       return todos;
-    });
-}
+export const deleteTodo = async(id) => {
+    const {error} =  await supabase.from('todos').delete().match({id});
+	
+    if (error) {
+		return console.error(error);
+	}
+    
+    todos.update((todos) => todos.filter((todo) => todo.id !== id));
+};
+
+export const toggleTodoCompleted = async(id, currentState) => {
+
+    const {error} =  await supabase.from('todos').update({completed: !currentState}).match({id});
+	
+    if (error) {
+		return console.error(error);
+	}
+
+	todos.update((todos) => {
+		let index = -1;
+		for (let i = 0; i < todos.length; i++) {
+			if (todos[i].id === id) {
+				index = i;
+				break;
+			}
+		}
+		if (index !== -1) {
+			todos[index].completed = !todos[index].completed;
+		}
+		return todos;
+	});
+};
