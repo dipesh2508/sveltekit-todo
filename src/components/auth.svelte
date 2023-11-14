40c1e38f004a2b7dcdfc00e2135dd05834f76bf9
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2023-11-14 20:48:05 +0530

    added auth form

--- src/components/auth.svelte
+++ src/components/auth.svelte
@@ -1,9 +1,43 @@
 <script>
-    import {supabase} from '../supabase.js';
+	import { supabase } from '../supabase.js';
 
-    let loading = false;
-    let email;
+	let loading = false;
+	let email;
+
+	const handleLogin = async () => {
+		try {
+			console.log(email);
+			const { error } = await supabase.auth.signIn({ email });
+
+			if (error) {
+				throw error;
+			}
+
+			alert('Check your email for the login link!');
+		} catch (error) {
+			console.error(error);
+			alert(error.error_description || error.message);
+		} finally {
+			loading = false;
+		}
+	};
 </script>
 
 <h1 class="text-2xl font-bold text-center text-slate-800 md:text-3xl">Log In</h1>
 <p class="text-center mt-2">Sign in via magic link with your email below</p>
+
+<form on:submit|preventDefault={handleLogin}>
+	<div class="flex flex-col text-sm mb-2">
+		<label for="email" class="font-bold mb-2 text-slate-800">Email</label>
+		<input
+			name="email"
+			class="appearance-none shadow-sm border border-slate-200 p-2 focus:outline-none focus:border-slate-500 rounded-lg"
+			type="email"
+            placeholder="Your email"
+            bind:value={email}
+		/>
+	</div>
+    <button type="submit" class="w-full shadow-sm rounded bg-emerald-500 hover:bg-emerald-600 text-white py-2 px-4">
+Log In
+    </button>
+</form>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
     import {supabase} from '../supabase';    
     import "../app.css";
+    import Auth from '../components/auth.svelte'
     console.log(supabase);
   </script>
   
   <div class="container mx-auto my-6 max-w-lg">
+    <Auth />
       <slot></slot>
   </div>
   
